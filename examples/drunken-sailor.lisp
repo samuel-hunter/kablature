6581636d6b70436c643c3e368ea39af8c58422a0
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-19 11:50:20 -0700

    Drafted set of intended tablature files to work with

---
+++ examples/drunken-sailor.lisp
@@ -0,0 +1,40 @@
+(deftablature "Drunken Sailor" (:keys 17 :timesig (2 . 4))
+  (4 nil 6) (8 nil 6) (8 nil 6)
+  (4 nil 6) (8 nil 6) (8 nil 6)
+  (4 nil 6) (4 nil 2)
+  (4 nil 4) (4 nil 6)
+
+  (4 nil 5) (8 nil 5) (8 nil 5)
+  (4 nil 5) (8 nil 5) (8 nil 5)
+  (4 nil 5) (4 nil 1)
+  (4 nil 3) (4 nil 5)
+
+  (4 nil 6) (8 nil 6) (8 nil 6)
+  (4 nil 6) (8 nil 6) (8 nil 6)
+  (4 nil 6) (4 nil 7)
+  (4 nil 8) (4 nil 9)
+
+  (4 nil 8) (4 nil 6)
+  (4 nil 5) (4 nil 3)
+  (2 nil 2)
+  (2 nil 2)
+
+  (2 nil 6)
+  (4 t 6) (8 nil 6)
+  (4 nil 6) (4 nil 2)
+  (4 nil 4) (4 nil 6)
+
+  (2 nil 5)
+  (4 t 5) (8 nil 5)
+  (4 nil 5) (4 nil 1)
+  (4 nil 3) (4 nil 5)
+
+  (2 nil 6)
+  (4 t 6) (8 nil 6)
+  (4 nil 6) (4 nil 7)
+  (4 nil 8) (4 nil 9)
+
+  (4 nil 8) (4 nil 6)
+  (4 nil 5) (4 nil 3)
+  (2 nil 2)
+  (2 nil 2))
