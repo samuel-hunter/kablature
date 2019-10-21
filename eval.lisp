7240459d659c18e252ab9bff3c1a12677b64d862
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-20 22:46:50 -0700

    Count measures within a tablature

--- eval.lisp
+++ eval.lisp
@@ -1,6 +1,23 @@
 (defpackage #:kablature.eval
-  (:use #:cl #:cl-svg)
+  (:use #:cl #:kablature.model)
   (:export :eval-kab))
 
+(in-package #:kablature.eval)
+
+;; FIXME: 4/4 timesignature is hardcoded in this procedure.
+(defun count-measures (tab)
+  "Count the measures within a tablature"
+  (loop :with duration := 0
+        :and measures := 0
+        :for construct :in (constructs kab)
+        :do (progn
+              (incf duration (duration construct))
+              (assert (<= duration 1))
+              (when (= duration 1)
+                (incf measures)
+                (setf duration 0)))
+        :finally (return measures)))
+
 (defun eval-kab (kab)
+  (format t "Measures: ~A~&" (count-measures kab))
   kab)
