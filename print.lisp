b9f74a9e3dbe4d88fb9d34f116fc900b3843096d
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-24 13:05:02 -0700

    Print chords.

--- print.lisp
+++ print.lisp
@@ -15,14 +15,16 @@
 (defparameter +measure-thickness+ 3)
 (defparameter +font-size+ 15)
 
+(defparameter +note-thickness+ 1)
 (defparameter +note-radius+ 4)
+(defparameter +stem-outreach+ 20)
 (defparameter +beat-height+ (* 2 +tabnote-width+))
+(defparameter +taper-length+ 5)
 
 (defparameter +tab-margin-x+ 50)
 (defparameter +tab-margin-y+ 10)
 (defparameter +measure-text-margin+ 5)
 
-(defparameter +thin-style+ "stroke-width:1;stroke:black")
 (defparameter +text-style+ (format nil "+font-size+:~A;fill:black" +font-size+))
 (defparameter +measure-color+ "black")
 
@@ -86,18 +88,24 @@
   "Return the bottom y postiion of the tab."
   (+ (tab-top) (tab-bar-height kab num-measures) +font-size+))
 
+(defun key-left (key num-keys)
+  "Return the x position of the left portion of the key."
+  (+ (tab-left) (* +tabnote-width+ (key-position key num-keys))))
+
+(defun key-center-x (key num-keys)
+  (+ (key-left key num-keys) (/ +tabnote-width+ 2)))
+
 (defun draw-tab-bar (kab scene num-measures)
   "Draw bars where notes will reside and text labels to signal the keys' notes."
   (loop :with num-keys := (keys kab)
         :with timesig := (timesig kab)
 
         :with tab-top := (tab-top)
-        :with tab-left := (tab-left)
         :with body-height := (tab-body-height timesig num-measures)
         :with body-bottom := (tab-body-bottom timesig num-measures)
 
         :for key :from 1 :upto num-keys
-        :for x := (+ tab-left (* +tabnote-width+ (key-position key num-keys)))
+        :for x := (key-left key num-keys)
         :for offset-height := (* +tabnote-offset-y+ (floor (- num-keys key) 2))
         :for markedp := (markedp key)
 
@@ -122,11 +130,88 @@
   (- (tab-body-bottom timesig tab-measures)
      (* measure-num (measure-height timesig))))
 
+(defgeneric draw-construct (scene construct y kab))
+
+(defmethod draw-construct (scene (beamed beamed) y kab)
+  (print "No support for beamed constructs yet."))
+
+(defun stem-left ()
+  (- (tab-left) +stem-outreach+))
+
+(defun rightmost-key (chord tab-keys)
+  (loop :for key :in (keys chord)
+        :with result := nil
+        :with result-pos := -1
+        :do (let ((pos (key-position key tab-keys)))
+              (when (> pos result-pos)
+                (setf result-pos pos)
+                (setf result key)))
+        :finally (return result)))
+
+(defun stem-right (chord tab-keys)
+  (key-center-x (rightmost-key chord tab-keys)
+                tab-keys))
+
+(defun stem-y (note-y)
+  "Return the y-position of the note's stem."
+  (+ (- note-y +note-radius+)))
+
+(defun draw-untapered-stem (scene chord tab-keys note-y)
+  (let ((stem-left (stem-left))
+        (stem-right (stem-right chord tab-keys))
+        (stem-y (stem-y note-y)))
+
+    (cl-svg:draw scene (:line :x1 stem-left :y1 stem-y
+                              :x2 stem-right :y2 stem-y)
+                 :stroke "black"
+                 :stroke-width +note-thickness+)))
+
+(defun draw-tapered-stem (scene chord tab-keys note-y)
+  (let ((stem-left (stem-left))
+        (stem-right (stem-right chord tab-keys))
+        (stem-y (stem-y note-y)))
+    (cl-svg:draw scene
+        (:polyline :points
+                   ;; points polyline attribute expect a "x,y x,y..."
+                   ;; string.
+                   (format nil "~$,~$ ~$,~$ ~$,~$"
+                           stem-right stem-y
+                           stem-left stem-y
+                           (+ stem-left +taper-length+)
+                           (- stem-y +taper-length+)))
+                 :stroke "black"
+                 :stroke-width +note-thickness+
+                 :fill "none")))
+
+(defmethod draw-construct (scene (chord chord) y kab)
+  (when (restp chord)
+    (print "No support for rests, yet.")
+    (return-from draw-construct))
+
+  ;; Appropriately draw the approrpiate chord stem in the appropriate
+  ;; style if so appropriate.
+  (ecase (note chord)
+    (1 nil) ; whole notes don't deserve a stem.
+    (2 (draw-untapered-stem scene chord (keys kab) y))
+    (4 (draw-untapered-stem scene chord (keys kab) y))
+    (8 (draw-tapered-stem   scene chord (keys kab) y)))
+
+  ;; Cord noteheads
+  (loop :with hollow-head := (member (note chord) '(1 2))
+        :for key :in (keys chord)
+        :do (cl-svg:draw scene (:circle :cx (key-center-x key (keys kab))
+                                        :cy y
+                                        :r +note-radius+)
+                         :fill (if hollow-head "none" "black")
+                         :stroke "black"
+                         :stroke-width +note-thickness+)))
+
 (defun draw-measure (scene kab measure-num tab-measures)
   "Draw the measure bar."
-  (let ((measure-bottom (measure-bottom measure-num (timesig kab) tab-measures))
-        (tab-left (tab-left))
-        (tab-right (tab-right (keys kab))))
+  (let* ((timesig (timesig kab))
+         (measure-bottom (measure-bottom measure-num timesig tab-measures))
+         (tab-left (tab-left))
+         (tab-right (tab-right (keys kab))))
 
     ;; measure line
     (cl-svg:draw scene (:line :x1 tab-left :x2 tab-right
@@ -136,7 +221,14 @@
     ;; measure label
     (cl-svg:text scene (:x (+ +measure-text-margin+ tab-right) :y measure-bottom
                         :style +text-style+ :dominant-baseline "middle")
-      (write-to-string (1+ measure-num)))))
+      (write-to-string (1+ measure-num)))
+
+    ;; measure constructs
+    (loop :for construct :in (nth measure-num (measures kab))
+          :with y := (- measure-bottom +beat-height+)
+          :do (draw-construct scene construct y kab)
+          :do (decf y (* +beat-height+
+                         (beat-length construct (beat-root timesig)))))))
 
 (defun print-kab (kab &optional (stream *standard-output*))
   (let* ((num-measures (length (measures kab)))
