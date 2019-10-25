3ec9995d6885d3ccb1f144df31ac63bc054d6adc
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-25 16:08:24 -0700

    examples: fix Hot Cross Buns inaccuracy

--- examples/hot-cross-buns.lisp
+++ examples/hot-cross-buns.lisp
@@ -1,6 +1,6 @@
 (deftablature "Hot Cross Buns" (:keys 17 :timesig (4 . 4))
-  (4 nil 5) (4 nil 2) (2 nil 2)
-  (4 nil 5) (4 nil 2) (2 nil 2)
+  (4 nil 5) (4 nil 2) (2 nil 5)
+  (4 nil 5) (4 nil 2) (2 nil 5)
   (8 nil 9) (8 nil 8) (8 nil 7) (8 nil 6) (8 nil 5) (8 nil 6) (8 nil 7) (8 nil 8)
   (4 nil 9) (4 nil 2) (2 nil 2)
 
