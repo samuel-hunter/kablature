7f5294939c061b6895a760e0453889cd9df378cc
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-25 16:53:55 -0700

    Examples: Add Happy Birthday

---
+++ examples/happy-birthday.lisp
@@ -0,0 +1,12 @@
+(deftablature "Happy Birthday" (:timesig (3 . 4) :keys 17)
+  (2 nil) (8 t 1) (16 nil 1)
+
+  (4 nil 2) (4 nil 1) (4 nil 4)
+  (2 nil 3) (8 t 1) (16 nil 1)
+  (4 nil 2) (4 nil 1) (4 nil 5)
+  (2 nil 4) (8 t 1) (16 nil 1)
+
+  (4 nil 8) (4 nil 6) (4 nil 4)
+  (4 nil 3) (4 nil 2) (8 t 7) (16 nil 7)
+  (4 nil 6) (4 nil 4) (4 nil 5)
+  (2 nil 4) (1 nil))
