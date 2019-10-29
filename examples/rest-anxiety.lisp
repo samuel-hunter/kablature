7694d9381348b178ace44a3d8fb77388d5aca527
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-28 19:47:07 -0700

    examples: add rest anxiety to test adding rests

---
+++ examples/rest-anxiety.lisp
@@ -0,0 +1,8 @@
+(deftablature "Rest Anxiety" (:keys 17 :timesig (4 . 4))
+  (1 nil)
+  (2 nil) (2 nil)
+  (4 nil) (4 nil) (4 nil) (4 nil)
+  (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil)
+
+  (2 t) (4 nil)
+  (4 t) (8 nil) (4 t) (8 nil))
