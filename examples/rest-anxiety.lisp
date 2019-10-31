3d7a5e5bcbb8e7f18d970edde5f70cbf374be746
Samuel Hunter <samuelhunter1024@gmail.com>
2019-10-31 14:10:26 -0700

    examples: modify rest anxiety to demonstrate all types of rests.

--- examples/rest-anxiety.lisp
+++ examples/rest-anxiety.lisp
@@ -1,8 +1,6 @@
 (deftablature "Rest Anxiety" (:keys 17 :timesig (4 . 4))
   (1 nil)
-  (2 nil) (2 nil)
-  (4 nil) (4 nil) (4 nil) (4 nil)
-  (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil) (8 nil)
+  (2 nil) (4 nil) (8 nil) (16 nil) (16 nil)
 
-  (2 t) (4 nil)
-  (4 t) (8 nil) (4 t) (8 nil))
+  (2 t) (8 t) (16 nil)
+  (2 nil) (4 t) (8 nil))
